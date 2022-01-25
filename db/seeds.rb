b5ff0ee87315e29f2a688dec93a75a95fdcc6df5
Melia Mazouzi <melia.mazouzi@gmail.com>
2022-01-25 18:31:30 +0100

    Add cv file to dowload

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "cleaning db..."
+User.destroy_all
+Project.destroy_all
+Contact.destroy_all
+
+puts "db cleaned!"
+
+puts "Creating user..."
+
+User.create!(
+  email: "melia.mazouzi@gmail.com",
+  password: "secret"
+)
+
+puts "User created!"
+
+puts "Creating project..."
+
+Project.create!(
+  name: "Planthy",
+  language: "Ruby on Rails"
+)
+
+Project.create!(
+  name: "Rez'event",
+  language: "Ruby on Rails"
+)
+
+Project.create!(
+  name: "Landing page",
+  language: "HTML"
+)
+
+Project.create!(
+  name: "To do list",
+  language: "Ruby on Rails"
+)
+
+puts "Project created!"
