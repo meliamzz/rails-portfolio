2aa8c93b46a0a0cd0884a2bcce202f4f2ecef0da
Melia Mazouzi <melia.mazouzi@gmail.com>
2022-01-26 15:33:34 +0100

    add cloudinary to upload project images

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,10 @@
 class ProjectsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
 end
