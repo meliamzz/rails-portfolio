2739cc776f0ce4b683418d6b2cbda217b7a489ca
Melia Mazouzi <melia.mazouzi@gmail.com>
2022-02-09 16:52:20 +0100

    fix ssl issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  skip_before_action :verify_authenticity_token, only: [:create]
+  # skip_before_action :verify_authenticity_token, only: [:create]
   before_action :authenticate_user!
 end
