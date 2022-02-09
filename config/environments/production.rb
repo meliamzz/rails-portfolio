4d82b51fda6b4b70e7147fc64118126d568afb3d
Melia Mazouzi <melia.mazouzi@gmail.com>
2022-02-09 13:49:58 +0100

    fix login with ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,11 +7,6 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  # Code for devise with SSL
-  config.to_prepare { Devise::SessionsController.force_ssl }
-  config.to_prepare { Devise::RegistrationsController.force_ssl }
-  config.to_prepare { Devise::PasswordsController.force_ssl }
-
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
